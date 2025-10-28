dc17a951747b956f9ec998cbb4e21d278225e068
August Bernberg <abearnmountain@gmail.com>
2025-10-28 15:09:33 -0400

    moved motor to another repo, will link that in docs

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ echo "Building Steering Wheel (Pico2)"
 cmake -B build/pico -DBUILD_STEERING=ON -DPICO_BOARD=pico2 steering_src/
 cmake --build build/pico -j4
 
-echo ""
-echo "Building Motor MCU (STM32F4)"
-cmake -B build/stm32 motor_src/
-cmake --build build/stm32 -j4
+# echo ""
+# echo "Building Motor MCU (STM32F4)"
+# cmake -B build/stm32 motor_src/
+# cmake --build build/stm32 -j4
 
 echo ""
 echo "Build Complete!"
 echo "Pico:  build/pico/steering_wheel.uf2"
-echo "STM32: build/stm32/motor_mcu.elf"
+# echo "STM32: build/stm32/motor_mcu.elf"
